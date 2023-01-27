96474a8ff83dc92a475cf79b2f3f0d162276b26a
cheo419 <theater914@gmail.com>
2023-01-28 08:46:17 +0900

    Nullable variables

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 void main() {
-  dynamic name;
-  if (name is String) {
-    name.
+  String? lee = 'cj';
+  lee = null;
+  // 이경우 lee가 null도 string이 될수있다고 한다면 ? 만 붙혀주면 됌
+
+  if (lee != null) {
+    lee.isNotEmpty;
   }
-  if (name is int){
-    name.
-  }
+  // =
+  lee?.isNotEmpty;
+  // 기본적으로 모든 변수는 non-nullable
 }
