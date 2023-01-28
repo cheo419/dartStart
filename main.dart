b1d1ace40a4ce3f51973bdbdd1b1b348ee60d00e
cheo419 <theater914@gmail.com>
2023-01-28 21:16:57 +0900

    String interpolation

--- main.dart
+++ main.dart
@@ -1,23 +1,7 @@
 void main() {
-  // var numbers = [
-  //   1,
-  //   2,
-  //   3,
-  //   4,
-  // ];
-  // List<int> numbers2 = [1, 2, 3, 4];
-  // == same
-
-  var givemeFour = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    if (givemeFour) 4,
-  ];
-
-  if (givemeFour) {
-    numbers.add(4);
-  }
-  // == same
+  var name = 'leecj';
+  var age = 27;
+  var greeting =
+      'Hello everyone, my name is $name and I\'m ${age + 2} hellyeah~~!!';
+  print(greeting);
 }
