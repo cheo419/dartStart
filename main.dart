df94ab349933cbaf6c904d54452fdb581515f81d
cheo419 <theater914@gmail.com>
2023-01-28 17:40:59 +0900

    Basic data types

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
-// 변수를 만드는 두가지 방법
 void main() {
-  var name = "pizza"; // 방법 1
-  name = "chicken ";
-  String name2 = "chicken"; // 방법 2
+  String name = "lee";
+  bool bol = true;
+  int age = 10;
+  double money = 52.55;
+  num x = 12;
+  num y = 1.2;
 }
-// 타입을 쓰지않을시 컴파일이 타입을 추론함
-// ex) name = 'lee'  ==> String 으로 추론
