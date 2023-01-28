fff7ca2d9602b48dc6260ba57e3fcd40595c983f
cheo419 <theater914@gmail.com>
2023-01-28 09:02:11 +0900

    Final variables

--- main.dart
+++ main.dart
@@ -1,12 +1,4 @@
 void main() {
-  String? lee = 'cj';
-  lee = null;
-  // 이경우 lee가 null도 string이 될수있다고 한다면 ? 만 붙혀주면 됌
-
-  if (lee != null) {
-    lee.isNotEmpty;
-  }
-  // =
-  lee?.isNotEmpty;
-  // 기본적으로 모든 변수는 non-nullable
+  final name = 'lee';
+  name = 'lee'; // error , 딱한번만 설정가능
 }
