c806e2fd6662ff6e40874f41ea4c6351275634ae
cheo419 <theater914@gmail.com>
2023-01-28 12:59:42 +0900

    복습

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
+// 변수를 만드는 두가지 방법
 void main() {
-  const max_allowed_price = 120;
+  var name = "pizza"; // 방법 1
+  name = "chicken ";
+  String name2 = "chicken"; // 방법 2
 }
+// 타입을 쓰지않을시 컴파일이 타입을 추론함
+// ex) name = 'lee'  ==> String 으로 추론
