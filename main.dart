5c7c75058101b02118a394a8b31eec0bf5ed5561
cheo419 <theater914@gmail.com>
2023-01-30 08:27:47 +0900

    Named constructors

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 class Player {
   final String name;
-  int age;
+  int age, killpoint;
   String team;
-  int killpoint;
 
   Player(
       {required this.name,
@@ -10,23 +9,35 @@ class Player {
       required this.team,
       required this.killpoint});
 
+  // Player를 초기화 하는 method
+  Player.createBlackPlayer({
+    required String name,
+    required int killpoint,
+  })  : this.killpoint = killpoint,
+        this.name = name,
+        this.team = 'black',
+        this.age = 0;
+
+  Player.createWhitePlayer(String name, int killpoint)
+      : this.killpoint = killpoint,
+        this.name = name,
+        this.team = 'white',
+        this.age = 0;
+
   void sayName() {
     print("hi my name is $name");
   }
 }
 
 void main() {
-  var player = Player(
-    name: "lee",
-    age: 29,
-    team: "red",
-    killpoint: 5,
+  // named 형식
+  var player = Player.createBlackPlayer(
+    name: "leecj",
+    killpoint: 14,
   );
-  var player2 = Player(
-    name: "gyu",
-    age: 62,
-    team: "green",
-    killpoint: 9,
+  // positional 형식
+  var player2 = Player.createWhitePlayer(
+    "leegyu",
+    21,
   );
-  player2.sayName();
 }
