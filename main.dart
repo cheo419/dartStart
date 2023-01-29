b5ee16b655122410923cb4376970ae209af051c4
cheo419 <theater914@gmail.com>
2023-01-30 01:16:33 +0900

    Sets

--- main.dart
+++ main.dart
@@ -1,28 +1,14 @@
 void main() {
-  // var player = {
-  //   'name': 'lee',
-  //   'xp' : 19.99,
-  //   'superpower' : false,
-  // };
-
-  // Map<int, bool> player = {
-  //   1: true,
-  //   2: false,
-  //   3: true,
-  // };
-
-  // Map<List<int>, bool> player = {
-  //   [1, 2, 3, 5]: true,
-  // };
-
-  // List<Map<String, Object>> players = [
-  //   {
-  //     'name': 'lee',
-  //     'xp': 19999.333,
-  //   },
-  //   {
-  //     'name': 'lee',
-  //     'xp': 19999.333,
-  //   },
-  // ];
+  // Set<int> numbers = {1, 2, 3, 4};
+  // numbers.add(1);
+  // numbers.add(1);
+  // numbers.add(1);
+  // print(numbers);
+  // 결과 : {1,2,3,4}
+  List<int> numbers = [1, 2, 3, 4];
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
+  // 결과 : [1,2,3,4,1,1,1]
 }
