c2c2c73e294663f33d0978ec6103903ea42c409a
cheo419 <theater914@gmail.com>
2023-01-30 10:39:00 +0900

    Cascade notation

--- main.dart
+++ main.dart
@@ -1,12 +1,13 @@
 class Player {
-  final String name;
+  String name;
   int age;
   String team;
 
-  Player.fromJson(Map<String, dynamic> playerJson)
-      : name = playerJson['name'],
-        age = playerJson['age'],
-        team = playerJson['team'];
+  Player({
+    required this.name,
+    required this.age,
+    required this.team,
+  });
 
   void sayName() {
     print("hi my name is $name");
@@ -14,28 +15,12 @@ class Player {
 }
 
 void main() {
-  var apiData = [
-    {
-      "name": "leecj",
-      "team": "black",
-      "age": 0,
-    },
-    {
-      "name": "leegyu",
-      "team": "black",
-      "age": 0,
-    },
-    {
-      "name": "hahyun",
-      "team": "black",
-      "age": 0,
-    }
-  ];
-  apiData.forEach((playerJson) {
-    var player = Player.fromJson(playerJson);
-    player.sayName();
-    // 결과 :  hi my name is leecj
-    //         hi my name is leegyu
-    //         hi my name is hahyun
-  });
+  var lee = Player(name: 'lee', age: 29, team: 'white')
+    ..name = 'cj'
+    ..age = 1
+    ..team = 'black';
+  // lee.name = 'lee';
+  // lee.age = '29';
+  // lee.team = 'white';
+  // same
 }
