5e356cfeaa5274d0a6b05755affedec788d24859
cheo419 <theater914@gmail.com>
2023-01-30 17:57:07 +0900

    Inheritance (상속)

--- main.dart
+++ main.dart
@@ -1,46 +1,33 @@
-abstract class Human {
-  void walk();
-}
-
-enum Team { black, white }
-
-enum Num { one, two }
-
-class Player extends Human {
-  String name;
-  Num age;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.age,
-    required this.team,
-  });
-
-  void walk() {
-    print("i'm walking");
-  }
-
+class Human {
+  final String name;
+  Human({required this.name});
   void sayName() {
     print("hi my name is $name");
   }
 }
 
-class Coach extends Human {
-  void walk() {
-    print('the coach is walking');
+enum Team { white, black }
+
+// extends 확장
+class Player extends Human {
+  final Team team;
+  // super 클래스는 확장한 부모클래스를 의미
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+  // override 대체
+  @override
+  void sayName() {
+    // super 확장(상속)한 부모클래스의 프로퍼티에 접근하게 하거나 메소드를 호출할수있게 해줌
+    super.sayName();
+    print('and I play for ${team}');
   }
 }
 
 void main() {
-  var lee = Player(
-    name: 'lee',
-    age: Num.one,
+  var player = Player(
     team: Team.white,
+    name: 'leecj',
   );
-  var cj = lee
-    ..name = 'cj'
-    ..age = Num.two
-    ..team = Team.black
-    ..sayName();
 }
