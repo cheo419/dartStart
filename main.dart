55eb35972232eed5bd507561e625f4329ff7b2b1
cheo419 <theater914@gmail.com>
2023-01-30 12:02:05 +0900

    Eunms

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
+// "" 이것처럼 텍스트형태로 쓸필요가 없음
+enum Team { black, white }
+
+enum Num { one, two }
+
 class Player {
   String name;
-  int age;
-  String team;
+  Num age;
+  Team team;
 
   Player({
     required this.name,
@@ -15,12 +20,30 @@ class Player {
 }
 
 void main() {
-  var lee = Player(name: 'lee', age: 29, team: 'white')
+  var lee = Player(
+    name: 'lee',
+    age: Num.one,
+    team: Team.white,
+  );
+  var cj = lee
     ..name = 'cj'
-    ..age = 1
-    ..team = 'black';
-  // lee.name = 'lee';
-  // lee.age = '29';
-  // lee.team = 'white';
-  // same
+    ..age = Num.two
+    ..team = Team.black
+    ..sayName();
 }
+
+
+// 또다른 예제
+
+// enum Team {
+// red(1), blue(3);
+
+// final int value;
+
+// const Team(this.value);
+// }
+
+// void main(){
+//   print(Team.blue.index); // 2
+//   print(Team.blue.value); // 3
+// }
